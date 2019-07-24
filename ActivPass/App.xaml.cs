abeacbd7bc85bc66c092ecf45af70b753b0642f3
henrik-peters <peters-henrik@outlook.de>
2019-07-24 20:54:08 +0200

    Inital commit

---
+++ ActivPass/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ActivPass
+{
+    /// <summary>
+    /// Applicationlogic for "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ActivPass/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("ActivPass")]
+[assembly: AssemblyDescription("ActivPass is a simple and local password management tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ActivPass")]
+[assembly: AssemblyCopyright("Copyright © 2019 Henrik Peters")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+[assembly: AssemblyVersion("0.1.0.*")]
