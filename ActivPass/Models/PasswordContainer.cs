29abea7d77c5b0811c89151f0eb16a447d242588
henrik-peters <peters-henrik@outlook.de>
2019-08-29 13:16:27 +0200

    Password container model defintions

---
+++ ActivPass/Models/PasswordContainer.cs
@@ -0,0 +1,26 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using System;
+
+namespace ActivPass.Models
+{
+    [Serializable]
+    public class PasswordContainer
+    {
+        public string ContainerName { get; set; }
+        public string Owner { get; set; }
+
+        public PasswordItem[] Items { get; set; }
+
+        public PasswordContainer(string ContainerName, string Owner)
+        {
+            this.ContainerName = ContainerName;
+            this.Owner = Owner;
+
+            this.Items = new PasswordItem[0];
+        }
+    }
+}
---
+++ ActivPass/Models/PasswordItem.cs
@@ -0,0 +1,25 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using System;
+
+namespace ActivPass.Models
+{
+    [Serializable]
+    public class PasswordItem
+    {
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public PasswordItem(string name, string username, string password)
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+        }
+    }
+}
