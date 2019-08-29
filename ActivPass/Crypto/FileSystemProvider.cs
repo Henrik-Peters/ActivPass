0f283c61bdc4ecb35a4f59ae0e6274e6a1484c8f
henrik-peters <peters-henrik@outlook.de>
2019-08-29 13:47:07 +0200

    Container storage interface

---
+++ ActivPass/Crypto/FileSystemProvider.cs
@@ -0,0 +1,42 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActivPass.Configuration;
+using ActivPass.Models;
+
+namespace ActivPass.Crypto
+{
+    public class FileSystemProvider : IContainerProvider
+    {
+        /// <summary>
+        /// Containers will be stored in the
+        /// file system in this directory path.
+        /// </summary>
+        private string StoragePath
+        {
+            get => ConfigProvider.AppDataPath;
+        }
+
+        public PasswordContainer LoadContainer(string containerName, string masterKey)
+        {
+            return null;
+        }
+
+        public bool SaveContainer(PasswordContainer container, string masterKey)
+        {
+            if (!Directory.Exists(StoragePath)) {
+                Directory.CreateDirectory(StoragePath);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ActivPass/Crypto/IContainerProvider.cs
@@ -0,0 +1,32 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using ActivPass.Models;
+
+namespace ActivPass.Crypto
+{
+    public interface IContainerProvider
+    {
+        /// <summary>
+        /// Load an encrypted container from the storage
+        /// and decrypt it with the master key. When the
+        /// operation has failed null will be returned.
+        /// </summary>
+        /// <param name="containerName">Name of the container</param>
+        /// <param name="masterKey">Decrypt the container with this key</param>
+        /// <returns>Decrypted container or null</returns>
+        PasswordContainer LoadContainer(string containerName, string masterKey);
+
+        /// <summary>
+        /// Save a container to the storage. Existing container
+        /// data will be overwritten. The container will be
+        /// encrypted with the master key.
+        /// </summary>
+        /// <param name="container">Container to encrypt and save</param>
+        /// <param name="masterKey">Key for the encryption</param>
+        /// <returns>True when the operation was sucessful</returns>
+        bool SaveContainer(PasswordContainer container, string masterKey);
+    }
+}
