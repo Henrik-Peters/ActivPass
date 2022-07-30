f5fb7c6b337e1d0f38ba8e832c78c79c8fda7183
Henrik-Peters <peters-henrik@outlook.de>
2022-07-30 21:53:57 +0200

    Password generator base view model implementation

--- ActivPass/ViewModels/PassGeneratorViewModel.cs
+++ ActivPass/ViewModels/PassGeneratorViewModel.cs
@@ -3,12 +3,10 @@
 // Copyright 2019 Henrik Peters
 // See LICENSE file in the project root for full license information
 #endregion
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using ActivPass.Localization;
+using ActivPass.Models;
 
 namespace ActivPass.ViewModels
 {
@@ -18,5 +16,104 @@ namespace ActivPass.ViewModels
         /// Reference for bindings to the translate singleton.
         /// </summary>
         public TranslateManager Localize => TranslateManager.GetTranslateManager();
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    //Apply the new value
+                    _password = value;
+                    NotifyPropertyChanged(nameof(Password));
+
+                    //Update the password score
+                    this.UpdatePasswordScore();
+                }
+            }
+        }
+
+        private PasswordStrength _passwordStrength;
+        public PasswordStrength PasswordStrength
+        {
+            get => _passwordStrength;
+            set
+            {
+                if (_passwordStrength != value)
+                {
+                    _passwordStrength = value;
+                    NotifyPropertyChanged(nameof(PasswordStrength));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Close the passed window instance.
+        /// </summary>
+        public ICommand Close { get; set; }
+
+        /// <summary>
+        /// Copy the argument as unicode text to the clipboard.
+        /// </summary>
+        public ICommand CopyToClipboard { get; set; }
+
+        /// <summary>
+        /// Generate a new random password with the current options.
+        /// </summary>
+        public ICommand GenerateNextPassword { get; set; }
+
+        public PassGeneratorViewModel()
+        {
+            //Initial values
+            this.GeneratePassword();
+
+            //Command bindings
+            this.Close = new RelayCommand<Window>(CloseWindow);
+            this.CopyToClipboard = new RelayCommand<string>(SetClipboardText);
+            this.GenerateNextPassword = new RelayCommand(GeneratePassword);
+        }
+
+        /// <summary>
+        /// Generate the next random password with
+        /// the current options in the view model
+        /// </summary>
+        private void GeneratePassword()
+        {
+            this.Password = "abc";
+        }
+
+        /// <summary>
+        /// Update the score rating of the current password
+        /// </summary>
+        public void UpdatePasswordScore()
+        {
+            //Calculate and apply the score
+            PasswordStrength score = PasswordScores.GetScore(this.Password);
+            this.PasswordStrength = score;
+        }
+
+        /// <summary>
+        /// Set a new text in the clipboard content.
+        /// The data text format will be set to unicode text.
+        /// </summary>
+        /// <param name="text">Copy this text to the clipboard</param>
+        private void SetClipboardText(string text)
+        {
+            Clipboard.SetText(text, TextDataFormat.UnicodeText);
+        }
+
+        /// <summary>
+        /// Close the passed window instance.
+        /// </summary>
+        /// <param name="window">Instance to close</param>
+        private void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
     }
 }
