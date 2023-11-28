58c78811bebdb781eb17658f01df39a2f2b2153a
Henrik-Peters <peters-henrik@outlook.de>
2023-11-28 21:38:07 +0100

    Strength report bar user control

---
+++ ActivPass/Controls/StrengthReportBarLegend.xaml.cs
@@ -0,0 +1,23 @@
+﻿#region License
+// MIT License
+// Copyright 2019 Henrik Peters
+// See LICENSE file in the project root for full license information
+#endregion
+using System.Windows.Controls;
+using ActivPass.Localization;
+
+namespace ActivPass.Controls
+{
+    public partial class StrengthReportBarLegend : UserControl
+    {
+        /// <summary>
+        /// Reference for bindings to the translate singleton.
+        /// </summary>
+        public static TranslateManager Localize => TranslateManager.GetTranslateManager();
+
+        public StrengthReportBarLegend()
+        {
+            InitializeComponent();
+        }
+    }
+}
