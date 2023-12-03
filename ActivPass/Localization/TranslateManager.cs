ab035c3afde76cc14585b03098e0432086be54b6
Henrik-Peters <peters-henrik@outlook.de>
2023-12-03 22:01:03 +0100

    Password editor unsafe url warn layout

--- ActivPass/Localization/TranslateManager.cs
+++ ActivPass/Localization/TranslateManager.cs
@@ -19,9 +19,9 @@ namespace ActivPass.Localization
         /// </summary>
         static IDictionary<string, List<string>> TranslateTable = new Dictionary<string, List<string>>
         {
-            
             ["AdditionalConfig"] = new List<string> { "Additional config", "Zusätzliche Konfiguration" },
             ["WeakPassword"] = new List<string> { "Weak password", "Schwaches Passwort" },
+            ["UnsafeUrl"] = new List<string> { "Unsafe Url", "Unsichere Url" },
             ["NonEncryptionTrafficUrl"] = new List<string> { "Url does not guarantee an encrypted connection", "Url garantiert keine verschlüsselte Verbindung" },
             ["Actions"] = new List<string> { "Actions", "Aktionen" },
             ["OpenContainerReport"] = new List<string> { "Report", "Sicherheitsbericht" },
